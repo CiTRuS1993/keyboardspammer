a3565225761f3dd90bf11c72ee7d0653566981a6
Igor <igoralikin@gmail.com>
2020-12-28 16:44:42 +0200

    changes

--- client.py
+++ client.py
@@ -3,49 +3,62 @@ import socket
 import scapy
 import parse
 import sys
+import select
 
 group_name = 'Team TCP/IP\n'
 wait_time = 10
 udp_port = 13117
 buffer_size = 4096
 tcp_port = 0
-sock = None
+myHostName = socket.gethostname()
+# server_ip = socket.gethostbyname(myHostName)
+server_ip = 'localhost'
 
 
 def start():
-	sock = socket(socket.AF_INET, socket.SOCK_DGRAM)
-	# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind(('', udp_port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', udp_port))
+    print("Client started, listening for offer requests...")
+    while True:
+        data, addr = sock.recvfrom(1024)
+        try:
+            tcp_port = int(parse.parse("feedbeef02{}", data.hex())[0], 16)
+            if tcp_port == 2118:
+                return addr[0], tcp_port  # The server address
+        except:
+            print("let's try again")
 
-	while True:
-		data, addr = sock.recvfrom(1024)
-		try:
-			tcp_port = int(parse.parse("feedbeef02{:d}",data.hex())[0], 16)
-			return addr  # The server address
-		except:
-			print("let's try again")
 
-def connect(addr,name):
-	print(f"Received offer from {addr}, attempting to connect...")
-	sock = socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((addr, tcp_port))
-	sock.setblocking(0)
-	sock.send(name)
+def connect(addr, name, port):
+    print(f"Received offer from {addr}, attempting to connect...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((addr, port))
+    # sock.setblocking(0)
+    sock.send(name.encode())
+    return sock
 
-def game():
-	while True:
-			msg,_ = sock.recv(buffer_size)
-			if msg:
-				print(msg)
-				break
-	while True:
-		msg,_ = sock.recv(buffer_size)
-		if msg:
-			print(msg)
-			break
-		sock.send(sys.stdin.read(1))
+
+def game(sock):
+    while True:
+        msg = sock.recv(buffer_size)
+        if msg:
+            print(msg)
+            break
+    while True:
+        readable, writable, exceptional = select.select([sock], [], [], 0)
+        if len(readable) > 0:
+            msg = sock.recv(buffer_size)
+            if msg:
+                print(msg)
+                break
+        sock.send(sys.stdin.read(1).encode())
+
 
 while True:
-	addr = start()
-	connect(addr,group_name)
-	game()
+    addr, port = start()
+    sock = connect(addr, group_name, port)
+    game(sock)
+    user_input = input("type exit to end game otherwise type anything")
+    if (user_input == 'exit'):
+        break
--- server.py
+++ server.py
@@ -3,45 +3,55 @@ import _thread
 import time
 import random
 import socket
-#import scapy
+import parse
+from scapy.arch import get_if_addr
+import select
 
+num_of_threads = []
 tcp_port = 2118
-server_name = 'HackerzServer\n'
 wait_time = 10
 udp_port = 13117
 buffer_size = 4096
-game_status = False
+# global game_status
+game_status = {'stat': False}
 score = {}
-groups = {'Group 1': list(), 'Group 2': list()}
+groups = {'Group 1': [], 'Group 2': []}
 myHostName = socket.gethostname()
-server_ip = socket.gethostbyname(myHostName)
-# server_ip = scapy.get_if_addr('eth1')
-
+# server_ip = socket.gethostbyname(myHostName)
+server_ip = get_if_addr('eth0')
+# server_ip = 'localhost'
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-udp_socket.bind(('', udp_port))
+# udp_socket.bind((server_ip, udp_port))
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.bind(('', tcp_port))
+tcp_socket.bind((server_ip, tcp_port))
 tcp_socket.listen(1)
 
 
 def start():
     while True:
-        game_status = False
-        groups['Group 1'] = {}
-        groups['Group 2'] = {}
+        # game_status['stat'] = False
+        groups['Group 1'] = []
+        groups['Group 2'] = []
         score = {}
         try:
-            _thread.start_new_thread(tcp_master,())
+            num_of_threads.append(1)
+            _thread.start_new_thread(tcp_master, ())
         except Exception as err:
             print(err)
         try:
-            _thread.start_new_thread(udp_broadcast,())
+            num_of_threads.append(1)
+            _thread.start_new_thread(udp_broadcast, ())
         except Exception as err:
             print(err)
 
         time.sleep(20)
-        if (input("To quit enter 'quit' text otherwise enter anything") == 'quit'):
+
+        while len(num_of_threads) > 0:
+            pass
+
+        user_input = input("To quit enter 'quit' text")
+        if (user_input == 'quit'):
             break
 
 
@@ -49,8 +59,10 @@ def udp_broadcast():
     start_time = time.time()
     while(time.time() - start_time <= 10):
         msg = bytes.fromhex('feedbeef02') + \
-                            (tcp_port).to_bytes(2, byteorder='big')
+            (tcp_port).to_bytes(2, byteorder='big')
         udp_socket.sendto(msg, ('<broadcast>', udp_port))
+    num_of_threads.pop()
+    return
 
 
 def tcp_master():
@@ -58,52 +70,64 @@ def tcp_master():
     name_list = list()
     start_time = time.time()
     while(time.time() - start_time <= 10):
-        client_socket, addr = tcp_socket.accept()
-        name = client_socket.recv(buffer_size)
-        name_counter = 1
-        while name in name_list:
-            name_counter += 1
-            name += str(name_counter)
-        groups['Group 1'].append((name, client_socket, addr)) if len(groups['Group 1']) >= len(
-            groups['Group 2']) else groups['Group 2'].append((name, client_socket, addr))
+        readable, writable, errored = select.select([tcp_socket], [], [], 0)
+        if len(readable) > 0:
+            client_socket, addr = tcp_socket.accept()
+            name = client_socket.recv(buffer_size).decode()
+            print(name)
+            name_counter = 1
+            while name in name_list:
+                name_counter += 1
+                name += str(name_counter)
+            groups['Group 1'].append((name, client_socket, addr)) if len(groups['Group 1']) >= len(
+                groups['Group 2']) else groups['Group 2'].append((name, client_socket, addr))
         # client_socket.settimeout(time.time() - start_time)
 
     start_game_message = 'Welcome to Keyboard Spamming Battle Royale.\n'
-    group_1_names = [name for name in groups['Group 1'].keys()]
-    group_2_names = [name for name in groups['Group 2'].keys()]
+    # group_1_names = [name for name in groups['Group 1']]
+    # group_2_names = [name for name in groups['Group 2']]
 
-    group_1_names_str = '\n'.join(group_1_names)
-    start_game_message += 'Group 1:\n==\n'
-    start_game_message += group_1_names_str
+    # group_1_names_str = '\n'.join(group_1_names)
+    # start_game_message += 'Group 1:\n==\n'
+    # start_game_message += group_1_names_str
 
-    group_2_names_str = '\n'.join(group_2_names)
-    start_game_message += 'Group 2:\n==\n'
-    start_game_message += group_2_names_str
+    # group_2_names_str = '\n'.join(group_2_names)
+    # start_game_message += 'Group 2:\n==\n'
+    # start_game_message += group_2_names_str
 
     for key, value in groups.items():
         for (name, socket, addr) in value:
+            num_of_threads.append(1)
             _thread.start_new_thread(
                 client_run, (name, socket, addr, start_game_message))
 
-    game_status = True
+    game_status['stat'] = True
     time.sleep(10)
-    game_status = False
+    game_status['stat'] = False
     print(get_scores())
+    num_of_threads.pop()
+    return
 
 
 def client_run(name, socket, addr, msg):
-    socket.send(msg)
+    socket.send(msg.encode())
     score[name] = 0
-    while not game_status:
+    while not game_status['stat']:
         pass
-    while game_status:
-        char = socket.recv(1024)
-        score[name] += 1
+    while game_status['stat']:
+        readable, writable, errored = select.select([socket], [], [], 0)
+        if len(readable) > 0:
+            char = socket.recv(1)
+            score[name] += 1
+    socket.send("finished".encode())
+    num_of_threads.pop()
+    return
 
 
 def get_scores():
     group1_score = sum([score[team] for (team, _, _) in groups['Group 1']])
     group2_score = sum([score[team] for (team, _, _) in groups['Group 2']])
     return group1_score, group2_score
+
 
 start()
